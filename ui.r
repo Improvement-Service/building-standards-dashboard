e387d768c89f45213d3b21ea16441e737eea2c0a
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2021-12-02 15:45:29 +0000

    Set up overview and question pages in sidebar

--- ui.r
+++ ui.r
@@ -1,5 +1,10 @@
 ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
+  dashboardHeader(title = "National Customer Survey Dashboard"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Performance Overview", tabName="PrfmncOvrvw", icon = icon("dashboard")),
+      menuItem("Question Results", tabName = "Qstns", icon = icon("question-circle"))
+    )
+  ),
   dashboardBody()
 )
