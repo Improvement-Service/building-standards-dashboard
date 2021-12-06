8340ba1dbd7e7018fe825abcf31ad80ecea2f29a
Cassidy <cassidy.nicholas@westlothian.gov.uk>
2021-12-06 15:39:01 +0000

    Draft code for creating valueBoxes, add LA names for drop-down

--- global.r
+++ global.r
@@ -1,3 +1,13 @@
 library(tidyverse)
 library(shiny)
 library(shinydashboard)
+
+#create a list of local authorty names for use in the UI
+LA_Names <- c("Aberdeen City", "Aberdeenshire","Angus", "Argyll and Bute" ,     
+                "Clackmannanshire","Dumfries and Galloway", "Dundee City",  "East Ayrshire",      
+                "East Dunbartonshire", "East Lothian", "East Renfrewshire","Edinburgh",     
+                "Na h-Eileanan an Iar", "Falkirk" , "Fife", "Glasgow City",       
+                "Highland", "Inverclyde","Midlothian","Moray",             
+                "North Ayrshire" ,"North Lanarkshire" ,"Orkney Islands","Perth and Kinross" ,   
+                "Renfrewshire", "Scottish Borders" ,"Shetland Islands" ,"South Ayrshire" ,    
+                "South Lanarkshire" ,"Stirling","West Dunbartonshire", "West Lothian")
--- server.r
+++ server.r
@@ -1,8 +1,21 @@
 server <- function(input, output) {
   
+#Create performance box for selected Council  
   output$performanceBox <- renderValueBox({
     valueBox(
-      value = "8.2", "Out of 10", icon = icon("chart-bar"), color = "blue"
+      value = "8.1", "Council Satisfaction YTD", icon = icon("chart-bar"), color = "red"
     )
   })
+#Create performance box for Scotland
+  output$scotPerfBox<- renderValueBox({
+    valueBox(
+      value = "8.2", "Scotland Average", icon = icon("times"), color = "blue"
+    )
+  })
+#Create responses valuebox
+    output$respBox <- renderValueBox({
+      valueBox(
+        value = paste("500", "Responses"), "xxx Year to Date", icon = icon("user-friends"), color = "green"
+      )
+    })
 }
