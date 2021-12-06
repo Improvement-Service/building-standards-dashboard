b8c07a9cf331989047732241b09fe59f3c447e2d
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2021-12-06 15:13:05 +0000

    Add new pages in sidebar

--- ui.r
+++ ui.r
@@ -2,9 +2,21 @@ ui <- dashboardPage(
   dashboardHeader(title = "National Customer Survey Dashboard"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Performance Overview", tabName="PrfmncOvrvw", icon = icon("dashboard")),
-      menuItem("Question Results", tabName = "Qstns", icon = icon("question-circle"))
+      menuItem("Performance Overview", tabName="PrfOvr", icon = icon("dashboard")),
+      menuItem("Question Results", tabName = "Qstns", icon = icon("question-circle")),
+      menuItem("Report Download", tabName = "RptDl", icon = icon("chart-area")),
+      menuItem("Data Download", tabName = "DtDl", icon = icon("download")),
+      menuItem("Open Text", tabName = "OpTxt", icon = icon("comments"))
     )
   ),
-  dashboardBody()
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "PrfOvr",
+              h2("KPO4 Performance"), ###add something to say date most recently updated?
+              valueBoxOutput("performanceBox")), 
+      tabItem(tabName = "Qstns",
+              h2("Performance by Question"))
+    )
+    
+  )
 )
