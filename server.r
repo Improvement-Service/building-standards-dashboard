5ff304d2ebb5e290699868b043da12cdcfaa6306
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2021-12-06 15:13:25 +0000

    Add draft infobox

--- server.r
+++ server.r
@@ -1,1 +1,8 @@
-server <- function(input, output) { }
+server <- function(input, output) {
+  
+  output$performanceBox <- renderValueBox({
+    valueBox(
+      value = "8.2", "Out of 10", icon = icon("chart-bar"), color = "blue"
+    )
+  })
+}
