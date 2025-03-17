4191c6311f64ec33a98ecc196664157c8274fc2a
Nick Cassidy <nicholas.cassidy@improvementservice.org.uk>
2025-03-17 10:40:43 +0000

    Update data_getter.py
    Update id

--- data_getter.py
+++ data_getter.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     api_key = config['DEFAULT']['api_key']
     api_secret_key = config['DEFAULT']['api_secret_key']
-    survey_id = config['DEFAULT']['survey_id']
+    survey_id = config['DEFAULT'][1029469]
     survey_name = 'Shiny App Export'
     base_url = "https://api.smartsurvey.io/v2/surveys"
     ### END ###
